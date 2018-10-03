594346c6d89c7c15cc40e74ab6ffc2ae483c5820
KillianSupinski <41280857+KillianSupinski@users.noreply.github.com>
2018-10-03 15:30:03 +0200

    insertion model_inscription/test insertion variable

--- application/controllers/ctrlInscription.php
+++ application/controllers/ctrlInscription.php
@@ -1,10 +1,18 @@
 <?php 
 class ctrlInscription extends CI_Controller
 {
-    public function connection()
+    public function index()
     {
+        
         $this->load->view('inscription');
         
+    }
+    public function getInscription($vNom, $vLogin, $vMdp)
+    {
+        
+        $this->load->model('Model_Inscription');
+        $data['lesInscription'] = $this->Model_Inscription->insertInscription($vNom, $vLogin, $vMdp);
+        $this->load->view('inscription');
     }
 }
     
--- application/views/inscription.php
+++ application/views/inscription.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
@@ -9,16 +9,40 @@
     <script src="main.js"></script>
 </head>
 <body>
+<?php
+
+    if (isset($_POST['btn'])) {             
+        if (!$_POST['txtLogin'] == "") { // on verifie que le nom est bien noté   
+              if(!$_POST['txtNom'] == ""){
+                    if (!$_POST['txtMdp'] == "") { // on verifie que le le mot de passe est bien noté
+                        if ($_POST['txtMdp2'] == $_POST['txtMdp']) { // on verifie que le mot de passe confirmé correspond
+                            header('location:index.php/ctrlInscription/getInscription?vNom='.$_POST['txtNom'].'&vMdp='.$_POST['txtLogin'].'&vLogin='.$_POST['txtLogin']);
+                        } else {
+                            echo "Veuillez confirmer votre mot de passe";
+                        }
+                    } else {
+                        echo "Veuillez rentrer votre mot de passe";
+                    }
+                }else {
+                    echo "Veuillez rentrer votre nom";
+                }
+        } else {
+            echo "Veuillez rentrer votre login";
+        }
+    } 
+
+?>
+
 <form method="POST" >
             
                 <h2>Inscription</h2>
              <br> <br><br> <br>
             <div id="DivInscription" >
-                <input id="nomEleve" type="text" name="txtNom" placeholder="Nom" class="placeholder">
-                <input id="prenomEleve" type="text" name="txtPrenom" placeholder="Prenom" class="placeholder"> <br/><br/>
-                <input id="emailEleve" type="email" name="txtMail" placeholder="Email" class="placeholder"> <br/><br/>
-                <input id="mdpEleve"type="password" name="txtMdp" placeholder="Mot de passe" class="placeholder">
-                <input id="mdpEleve2"type="password" name="txtMdp2" placeholder="Confirmer Mot de passe" class="placeholder"><br/> <br/>
+                <input id="idLogin" type="text" name="txtLogin" placeholder="Login" class="placeholder"> <br/><br/>
+                <input id="idNom" type="text" name="txtNom" placeholder="Nom" class="placeholder">
+                <input id="idMdp"type="password" name="txtMdp" placeholder="Mot de passe" class="placeholder">
+                <input id="idMdp2"type="password" name="txtMdp2" placeholder="Confirmer Mot de passe" class="placeholder"><br/> <br/>
+                <input type="submit" name="btn" >
             </div>
 
         </form>
