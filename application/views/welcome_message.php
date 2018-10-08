f092f0dab6f05136a3638d291e0c547773dceeea
Laura Klinger <laura.klinger@hotmail.fr>
2018-10-08 16:48:59 +0200

    blo

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,10 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<meta charset="utf-8">llllllllll
 
 	<style type="text/css">
+
 
 	::selection { background-color: #E13300; color: white; }
 	::-moz-selection { background-color: #E13300; color: white; }
