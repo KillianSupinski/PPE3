ad7a7b10500e6495a95a7ec3f00df1cbe15a429e
Laura Klinger <laura.klinger@hotmail.fr>
2018-12-19 18:36:40 +0100

    blabla

--- application/views/modifierOffre.php
+++ application/views/modifierOffre.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="<?php echo base_url(); ?>JQuery/jquery-3.1.1.js"></script>
+    <script src="<?php echo base_url(); ?>JQuery/jquery-ui.js"></script>
+    <script src="<?php echo base_url(); ?>JQuery/jquery-ui.min.js"></script>
     <script src="<?php echo base_url(); ?>JS/fonctionK.js"></script>
     <script src="<?php echo base_url(); ?>JS/fonction.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
@@ -17,10 +19,24 @@
     <link href="<?php echo base_url(); ?>css/styleL.css" rel="stylesheet">
 </head>
 <body>
-<form id="modOffre-form" class="modal">
-    <div id="divModalOffre" class="modal:open">
-        
-    </div>
-</form>
+    <form method="GET" id="modOffre-form" action = "<?php echo base_url(); ?>index.php/ctrl_Accueil/ModifierLOffre/" class="modal form1">
+        <h3>Modifier l'offre</h3>
+            <label>Liste des services disponibles</label>
+            <select class="form-control" id="modLOffre">
+                <?php
+                    foreach ($lesServicesOffres as $unService)
+                    {
+                ?>
+                        <option name="nomOffre" value="<?php echo $unService->idService ?>" ><?php echo $unService->nomService." - ".$unService->idService ; ?></option>
+                <?php
+                    }
+                ?>
+            </select>
+            <label>Description de l'offre</label>
+            <input type="textarea" class="form-control" id="descOffre" width="300px">
+            <br/>
+            <button type="button" name="btnModOffre" onclick="modificationOffre()">Valider la modification <i class="glyphicon glyphicon-ok"></i></button></div>
+        </div>
+    </form>
 </body>
-</html> 
+</html>
